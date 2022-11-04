cbe7a55e3203758008c439845128dea1bc6c86f5
Stefan Brunotte <mrbrunotte@gmail.com>
2022-11-04 13:58:38 +0100

    Exercise 3 klar

---
+++ Exe_3_Poly/MyOwnClass1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exe_3_Polymorfism
+{
+    internal class MyOwnClass1 : UserError
+    {
+        public override string UEMessage()
+        {
+            return "Text from MyOwnClass1";
+        }
+    }
+}
--- Exe_3_Poly/Program.cs
+++ Exe_3_Poly/Program.cs
@@ -17,5 +17,34 @@ instanser av NumericInputError och TextInputError.
 13. F: Vad är det för en skillnad på en Abstrakt klass och ett Interface?
  */
 
+
+/*
+         * 11) Nör man behärskar polymorfism så kommer man att skriva mindre kod.
+         * Vi behöver inte hitta på nya namn på metoder som i stort gör samma sak
+         * men endast skiljer sig i små detaljer.
+         *
+         * 12) Om man bygger in bra struktur så gör polymorfismen att man kommer
+         * skriva mindre kod eftersom man kan ändra de små detaljerna som skiljer
+         * men behålla den initala egenskapen en metod skall implementera.
+         *
+         * En abstract klass skapar funktionalitet som subklasser kan implementera
+         * eller skriva över (override).
+         * Ett interface definierar endast funktionaliteten och man kan inte implementera
+         * den som i den abstrakta klassen.
+         */
+using Exe_3_Polymorfism;
+
 Console.WriteLine("--- 3.4 Polymorfism ---\n");
 
+List<UserError> userErrors = new List<UserError>();
+
+userErrors.Add(new NumericInputError());
+userErrors.Add(new TextInputError());
+userErrors.Add(new MyOwnClass1());
+userErrors.Add(new MyOwnClass2());
+userErrors.Add(new MyOwnClass3());
+
+foreach(UserError error in userErrors)
+{
+    Console.WriteLine(error.UEMessage());
+}
