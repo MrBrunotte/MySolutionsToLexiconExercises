7c5a4b5a0b23bb228da7636467f720caa2fa0df6
Stefan Brunotte <mrbrunotte@gmail.com>
2022-11-07 16:29:28 +0100

    Garage 1.0

---
+++ Exe_4_Garage_1_0/Vehicle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exe_5_Garage_1_0
+{
+    internal class Vehicle
+    {
+        public string RegNumber { get; }
+        public string Color1 { get; }
+        public int NrOfWheels { get; }
+
+        public Vehicle(string regNumber, string color, int nrOfWheels)
+        {
+            RegNumber = regNumber;
+            Color1 = color;
+            NrOfWheels = nrOfWheels;
+        }
+    }
+}
