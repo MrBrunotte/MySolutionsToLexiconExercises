a1eb08aa4bebb68809d6adac3af50d6ee22d4319
Stefan Brunotte <mrbrunotte@gmail.com>
2022-11-10 13:32:29 +0100

    Video 6 Yield

---
+++ YieldReturn_game_video_5/Program.cs
@@ -0,0 +1,35 @@
+﻿
+
+
+
+internal class Program
+{
+    static void Main(string[ ] args)
+    {
+        Console.WriteLine("\nReturn with yeild");
+        foreach(var num in YieldClass.GetNumbers())
+        {
+            Console.WriteLine(num);
+        }
+        Console.WriteLine("\nReturn ordinary list");
+        foreach(var num in YieldClass.GetNumOrdinary())
+        {
+            Console.WriteLine(num);
+        }
+        Console.WriteLine("\nReturn with yield but only take 2 in the infinite loop");
+        foreach(var num in YieldClass.GetNumYield().Take(2))
+        {
+            Console.WriteLine(num);
+        }
+
+        // så funkar IEnumerator, hämtar en kopia på ett objekt och ittererar ígenom det objektet.
+        var result = YieldClass.GetNumbers();
+        IEnumerator<int> enumerator = result.GetEnumerator();
+
+        while(enumerator.MoveNext())
+        {
+            var item = enumerator.Current;
+            Console.WriteLine(item);
+        }
+    }
+}
---
+++ YieldReturn_game_video_5/YieldClass.cs
@@ -0,0 +1,28 @@
+﻿internal class YieldClass
+{
+    public static IEnumerable<int> GetNumOrdinary()
+    {
+        var i = 0;
+        var result = new List<int>();
+        while(i < 5)
+        {
+            result.Add(i++);
+        }
+        return result;
+    }
+    public static IEnumerable<int> GetNumbers()
+    {
+        yield return 1;
+        yield return 2;
+        yield return 3;
+    }
+    public static IEnumerable<int> GetNumYield()
+    {
+        var i = 0;
+        var result = new List<int>();
+        while(true)
+        {
+            yield return i++;
+        }
+    }
+}
